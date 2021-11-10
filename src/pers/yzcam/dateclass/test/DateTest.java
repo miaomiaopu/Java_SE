2dda302a748f8cb12237ee1d84a7fb4c04e4d37a
miaomiaopu <1991416497@qq.com>
2021-11-10 14:32:53 +0800

    13 Commit Date类的常用方法

--- src/pers/yzcam/dateclass/test/DateTest.java
+++ src/pers/yzcam/dateclass/test/DateTest.java
@@ -20,4 +20,27 @@ public class DateTest {
         Employee employee = dateSupport.createEmployee(name, birth);
         System.out.println("Name: " + employee.getName() + " Birth: " + employee.getBirth());
     }
+
+    @Test
+    void compareEmployeeBirthTest() {
+        Employee employee1 = new Employee("小红", new Date(System.currentTimeMillis() - 100));
+        Employee employee2 = new Employee("小刚", new Date(System.currentTimeMillis() - 101));
+        System.out.println(dateSupport.compareEmployeeBirth(employee1, employee2));
+    }
+
+    @Test
+    void changeDateStatusTest() {
+        Date date = new Date(123456789);
+        int tag = 1;
+        long num = 10000;
+        dateSupport.changeDateStatus(date, num, tag);
+        System.out.println(date.getTime());
+    }
+
+    @Test
+    void toStringForDateTest() {
+        Date date = new Date();
+
+        System.out.println(date);
+    }
 }
--- src/pers/yzcam/dateclass/utils/DateSupport.java
+++ src/pers/yzcam/dateclass/utils/DateSupport.java
@@ -18,4 +18,34 @@ public class DateSupport {
     public Employee createEmployee(String name, Date birth) {
         return new Employee(name, birth);
     }
+
+    /**
+     * 判断两个职员的出生日期先后
+     *
+     * @param employee1 职员1
+     * @param employee2 职员2
+     * @return 判断的字符串
+     */
+    public String compareEmployeeBirth(Employee employee1, Employee employee2) {
+        return employee1.getBirth().after(employee2.getBirth()) ?
+                employee1.getName() + " 比 " + employee2.getName() + " 出生更晚" :
+                "两人出生日期相同 或者 " + employee2.getName() + " 比 " + employee1.getName() + " 出生更晚";
+    }
+
+    /**
+     * 增加减少当前对象时间
+     *
+     * @param date 源时间对象
+     * @param num  增加减少的幅度
+     * @param tag  增加减少标志
+     * @return 修改完成的字符串
+     */
+    public Date changeDateStatus(Date date, long num, int tag) {
+        if (tag > 0) {
+            date.setTime(date.getTime() + num);
+        } else {
+            date.setTime(date.getTime() - num);
+        }
+        return date;
+    }
 }
